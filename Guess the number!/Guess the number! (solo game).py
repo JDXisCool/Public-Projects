e06f06064b8dddda2cde4ef24a7863e1cb95c147
JDXisCool <JDXGmaing@gmail.com>
2023-05-06 18:03:37 +0530

    Debugged a mistake i made

--- Guess the number!/Guess the number! (solo game).py
+++ Guess the number!/Guess the number! (solo game).py
@@ -1,7 +1,6 @@
 import random
 num = random.randint (1, 10)
 print (" The random numbers are generated from 1 to 10")
-While True
 input ("What is the number that you choose?")
 if input == num:
     print (" You are A good guesser!")
