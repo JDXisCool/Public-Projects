0d2673a008cd34a60ffe3556e578f22ff95178be
JDXisCool <JDXGmaing@gmail.com>
2023-05-06 17:51:25 +0530

    writing code for only player number guessing game

--- Learning Files/3.py
+++ Learning Files/3.py
@@ -1,9 +1,23 @@
 import random
-num = random.randint (1, 4)
+num = random.randint (1, 10)
+print (" The random numbers are generated from 1 to 10")
+input ("What is the number that you choose?")
+if input == num:
+    print (" You are A good guesser!")
+
+if input != num:
+    print ("The Number that you guessed is wrong. Better luck next time!")    
+
+print ("Thanks for playing!") 
+ans = input (" Do you want to play again? Type 1 or 2")
+print (" 1 means yes and 2 means no")
+if ans == 2:
+         num = random.randint (1, 10)
 input ("What is the number?")
 if input == num:
     print (" You are A good guesser!")
-    
 
-print ("Thanks for playing!")    
-   
+if input != num:
+    print ("The Number that you guessed is wrong. Better luck next time!")    
+
+print ("Thanks for playing!")
