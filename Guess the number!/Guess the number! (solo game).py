83fd3d7963d79235155048a949687e07345bc39d
JDXisCool <JDXGmaing@gmail.com>
2023-05-16 18:09:02 +0530

    Making a yes or no system (finaly)

--- Guess the number!/Guess the number! (solo game).py
+++ Guess the number!/Guess the number! (solo game).py
@@ -9,3 +9,17 @@ if input != num:
     print ("The Number that you guessed is wrong. Better luck next time!")    
 
 print ("Thanks for playing!") 
+repeat = input ("Would you like to play again? Type y or n : ")
+if repeat == "y":
+     while True:
+         num = random.randint (1, 10)
+print (" The random numbers are generated from 1 to 10")
+input ("What is the number that you choose?")
+if input == num:
+    print (" You are A good guesser!")
+
+if input != num:
+    print ("The Number that you guessed is wrong. Better luck next time!")    
+    
+print ("Thanks for playing!") 
+repeat = input ("Would you like to play again? Type y or n : ")
