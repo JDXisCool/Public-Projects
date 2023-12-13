ae194661c3b391e021aadec61d6cf08cff3b85a0
JDXisCool <JDXGmaing@gmail.com>
2023-12-13 20:28:09 +0530

    Used tabine ai to fix the entire thing

--- Calculator/cal.py
+++ Calculator/cal.py
@@ -1,28 +1,32 @@
 import random
-Operation = input ("Select one : + , - X , Divison : ")
-if Operation == "+":
-    number1 = float(input (" What is Number One? Please type here :"))
-    number2 = float(input (" What is Number Two? Please type here :"))
-    Answer = number1 + number2
-    print ("Your answer is " + str(Answer))
-
-elif Operation == "-":
-    number1 = float(input (" What is Number One? Please type here :"))
-    number2 = float(input (" What is Number Two? Please type here :"))
-    Answer = number1 - number2
-    print ("Your answer is " + str(Answer))
-      
-elif Operation == "*":
-    number1 = float(input (" What is Number One? Please type here :"))
-    number2 = float(input (" What is Number Two? Please type here :"))
-    Answer = number1 * number2
-    print ("Your answer is " + str(Answer)) 
-    
-elif Operation == "/": 
-    number1 = float(input (" What is Number One? Please type here :"))
-    number2 = float(input (" What is Number Two? Please type here :"))
-    if number2 != 0:
-        Answer = number1 / number2
+while True:
+    Operation = input("Select one : + , - X , Divison : ")
+    if Operation == "+":
+        number1 = float(input (" What is Number One? Please type here :"))
+        number2 = float(input (" What is Number Two? Please type here :"))
+        Answer = number1 + number2
         print ("Your answer is " + str(Answer))
-    else:
-        print("Error: Division by zero is not allowed.")
+
+    elif Operation == "-":
+        number1 = float(input (" What is Number One? Please type here :"))
+        number2 = float(input (" What is Number Two? Please type here :"))
+        Answer = number1 - number2
+        print ("Your answer is " + str(Answer))
+
+    elif Operation == "*":
+        number1 = float(input (" What is Number One? Please type here :"))
+        number2 = float(input (" What is Number Two? Please type here :"))
+        Answer = number1 * number2
+        print ("Your answer is " + str(Answer))
+
+    elif Operation == "/": 
+        number1 = float(input (" What is Number One? Please type here :"))
+        number2 = float(input (" What is Number Two? Please type here :"))
+        try:
+            Answer = number1 / number2
+            print ("Your answer is " + str(Answer))
+        except:
+            print("Error: Division by zero is not allowed.")
+            response = input("Please type y or n to use the program again : ")
+            if response == "n":
+                break
