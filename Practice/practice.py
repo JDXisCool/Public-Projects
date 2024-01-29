c589772aae62e3a814414894982e47b802576535
JDXisCool <JDXGmaing@gmail.com>
2024-01-29 16:24:29 +0530

    hi

---
+++ Practice/practice.py
@@ -0,0 +1,4 @@
+Average1 = float(input("Enter one number: "))
+Average2 = float(input("Enter another number: "))
+Average3 = float(input("Enter yet another number: "))
+print("The average is :", (Average1 + Average2 + Average3) / 3)
